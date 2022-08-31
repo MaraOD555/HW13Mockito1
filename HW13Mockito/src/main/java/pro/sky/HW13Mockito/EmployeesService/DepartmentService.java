a435cda6aba69ba3e31f34281590176c96a0b27b
MaraOD555 <ruffbc@hotmail.com>
2022-08-31 17:29:03 +0300

    ДЗ, тесты JUnit заваливаются

--- HW13Mockito/src/main/java/pro/sky/HW13Mockito/EmployeesService/DepartmentService.java
+++ HW13Mockito/src/main/java/pro/sky/HW13Mockito/EmployeesService/DepartmentService.java
@@ -2,10 +2,12 @@ package pro.sky.HW13Mockito.EmployeesService;
 
 import org.springframework.stereotype.Service;
 import pro.sky.HW13Mockito.Employees.Employees;
-
+import pro.sky.HW13Mockito.Exceptions.EmployeeNotFoundException;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @Service
 public class DepartmentService {
@@ -15,42 +17,31 @@ public class DepartmentService {
         this.employeesService = employeesService;
     }
 
-    public String getEmployeesByDepartment() {
-        List<Employees> getEmployeesByDepartment = employeesService.getAll().stream()
-                .sorted(Comparator.comparing(Employees::getDepartmentId))//сортировка в прямом направлении
-                //перебирая каждого сотрудника и номер отдела
-                .toList(); //новый лист
-
-       return getEmployeesByDepartment.toString();
-    }
-    public String getEmployeesInDepartment(String departmentId) {
-         List<Employees> getEmployeesInDepartment = employeesService.getAll().stream() // объявляем лист сотрудников
-                 // и создаем поток (stream)
-            .filter(employee -> Objects.equals(employee.getDepartmentId(), departmentId))// фильтр,
+    public Map<Integer, List<Employees>> getEmployeesByDepartment() {
+        return employeesService.getAll().stream() // и создаем поток (stream)
+                .collect(Collectors.groupingBy(Employees::getDepartmentId));
+        }
+    public List<Employees> getEmployeesInDepartment(int departmentId) {
+        return employeesService.getAll().stream() // и создаем поток (stream)
+            .filter(employee -> employee.getDepartmentId()==departmentId)// фильтр,
                  // который перебирает все "departmentId" и сравнивает у них отдел с искомым
-            .toList();
+            .collect(Collectors.toList());
 
-        return "Сотрудники отдела " + departmentId + ":\n" + getEmployeesInDepartment;
+      //  return "Сотрудники отдела " + departmentId + ":\n" + getEmployeesInDepartment;
     }
-    public String lowestSalaryInDepartment(String departmentId) {
-      return employeesService.getAll().stream()//String lowestSalaryInDepartment = employeesService.getAll().stream()
-                //оригинал переменной, идея преобразовала в тот вид, который сейчас
-                .filter(employee -> {
-                return Objects.equals(employee.getDepartmentId(), departmentId); // сначала перебор по отделам,
-                    // сравнивает его с искомым (задан в качестве аргумента)
-                })
-                .min(Comparator.comparingInt(employees -> employees.getSalaryOfEmployee()))// минимальное значение в отделе
+    public Employees lowestSalaryInDepartment(int departmentId) {
+      return employeesService.getAll().stream()
+                .filter(employee -> employee.getDepartmentId()== departmentId)
+                .min((Comparator.comparingDouble(Employees::getSalaryOfEmployee)))// минимальное значение в отделе
                 // через компаратор в лямбда выражении
-                .get().toString();
+                .orElseThrow(EmployeeNotFoundException::new);
     }
-    public String highestSalaryInDepartment(String departmentId) {
+    public Employees highestSalaryInDepartment(int departmentId) {
         return employeesService.getAll().stream()//String highestSalaryInDepartment = employeesService.getAll().stream()
                 //оригинал переменной, идея преобразовала в тот вид, который сейчас
-                .filter(employee -> {
-                    return Objects.equals(employee.getDepartmentId(), departmentId); // сначала перебор по отделам
-                })
-                .max(Comparator.comparingInt(Employees :: getSalaryOfEmployee))// минимальное значение в отделе
+                .filter(employee -> employee.getDepartmentId()== departmentId)
+                .max((Comparator.comparingDouble(Employees :: getSalaryOfEmployee)))// минимальное значение в отделе
                 // через компаратор в цикле
-                .get().toString(); // возвращаем одного че6ловекапосле фильтра и сравнения з/п
+                .orElseThrow(EmployeeNotFoundException::new);
     }
 }
